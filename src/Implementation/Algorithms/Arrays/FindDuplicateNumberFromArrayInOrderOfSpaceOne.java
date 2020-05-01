4373b96f75823d55b959d2e255baa51a212d751e
unknown <nitantlakhera90@gmail.com>
2020-05-01 19:02:48 +0530

    Implementation of Data Structure and algorithm

--- src/Implementation/Algorithms/Arrays/FindDuplicateNumberFromArrayInOrderOfSpaceOne.java
+++ src/Implementation/Algorithms/Arrays/FindDuplicateNumberFromArrayInOrderOfSpaceOne.java
@@ -7,12 +7,12 @@ public class FindDuplicateNumberFromArrayInOrderOfSpaceOne {
  }
  public static void findElement(int array[]) {
 	 for(int i = 0 ; i < array.length ; i++) {
-		 if(array[Math.abs(array[i])] >=0 ) {
+	   	 if(array[Math.abs(array[i])] >=0 ) {
 			 array[Math.abs(array[i])] = -array[Math.abs(array[i])];
 		 }
 		 else {
 			 System.out.println(Math.abs(array[i]));
 		 }
 	 }
- }
+   }
 }
